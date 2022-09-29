58cf77c671c26630abaed1b504ee8ece22d2ecf1
rohit_verma <987@Open>
2022-09-29 13:58:14 +0530

    fix linting

--- cql.go
+++ cql.go
@@ -1,12 +1,13 @@
-package xk6_cassandra
+package cql
 
 import (
 	"fmt"
+	"log"
+	"strings"
+
 	"github.com/gocql/gocql"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
-	"log"
-	"strings"
 )
 
 // init is called by the Go runtime at application startup.
@@ -40,7 +41,7 @@ func (cql *CQL) Exports() modules.Exports {
 }
 
 type Config struct {
-	Url      string `json:"url"`
+	URL      string `json:"url"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Timeout  string `json:"timeout"`
@@ -49,14 +50,14 @@ type Config struct {
 
 func (cql *CQL) Connect(config Config) (*gocql.Session, error) {
 	rt := cql.vu.Runtime()
-	if config.Url == "" {
+	if config.URL == "" {
 		common.Throw(rt, fmt.Errorf("url is required"))
 	}
 	if config.Timeout == "" {
 		config.Timeout = "10s"
 	}
-	hosts := strings.Split(config.Url, ",")
-	var cluster = gocql.NewCluster(hosts...)
+	hosts := strings.Split(config.URL, ",")
+	cluster := gocql.NewCluster(hosts...)
 	cluster.Authenticator = gocql.PasswordAuthenticator{Username: config.Username, Password: config.Password}
 	if config.DC != "" {
 		cluster.PoolConfig.HostSelectionPolicy = gocql.DCAwareRoundRobinPolicy(config.DC)
@@ -67,12 +68,14 @@ func (cql *CQL) Connect(config Config) (*gocql.Session, error) {
 	}
 	return session, nil
 }
+
 func (*CQL) Exec(session *gocql.Session, stmt string) error {
 	if err := session.Query(stmt).Exec(); err != nil {
 		log.Fatal(err)
 	}
 	return nil
 }
+
 func (*CQL) CheckTable(session *gocql.Session, keyspace string, table string) bool {
 	ks, err := session.KeyspaceMetadata(keyspace)
 	if err != nil {
@@ -84,39 +87,40 @@ func (*CQL) CheckTable(session *gocql.Session, keyspace string, table string) bo
 	}
 	return false
 }
+
 func (*CQL) Insert(session *gocql.Session, keyspace string, table string, col []string, vals []string) error {
-	stmt := fmt.Sprintf("INSERT INTO %s.%s ( %s ) VALUES ( '%s' );", keyspace, table, strings.Join(col[:], ","), strings.Join(vals[:], "', '"))
+	stmt := fmt.Sprintf("INSERT INTO %s.%s ( %s ) VALUES ( '%s' );", keyspace, table, strings.Join(col, ","), strings.Join(vals, "', '"))
 	if err := session.Query(stmt).Exec(); err != nil {
 		log.Fatal(err)
 	}
 	return nil
 }
 
-//func (*CQL) Query(session *gocql.Session, table string) error {
-//	var id gocql.UUID
-//	var text string
-//
-//	if err := session.Query(`SELECT id, text FROM ?  WHERE timeline = ? LIMIT 1`, table, "me").Consistency(gocql.One).Scan(&id, &text); err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Println("Tweet:", id, text)
-//	return nil
-//}
-//func (*CQL) Scan(session *gocql.Session, table string) error {
-//	var id gocql.UUID
-//	var text string
-//
-//	scanner := session.Query(`SELECT id, text FROM tweet WHERE timeline = ?`,
-//		"me").Iter().Scanner()
-//	for scanner.Next() {
-//		err := scanner.Scan(&id, &text)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		fmt.Println("Tweet:", id, text)
-//	}
-//	if err := scanner.Err(); err != nil {
-//		log.Fatal(err)
-//	}
-//	return nil
-//}
+/*func (*CQL) Query(session *gocql.Session, table string) error {
+	var id gocql.UUID
+	var text string
+
+	if err := session.Query(`SELECT id, text FROM ?  WHERE timeline = ? LIMIT 1`, table, "me").Consistency(gocql.One).Scan(&id, &text); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Tweet:", id, text)
+	return nil
+}
+func (*CQL) Scan(session *gocql.Session, table string) error {
+	var id gocql.UUID
+	var text string
+
+	scanner := session.Query(`SELECT id, text FROM tweet WHERE timeline = ?`,
+		"me").Iter().Scanner()
+	for scanner.Next() {
+		err := scanner.Scan(&id, &text)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Tweet:", id, text)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}*/
